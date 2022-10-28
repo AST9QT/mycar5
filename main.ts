3691e7cb9cc6d319ea2275f706566f999d891a02
AST <32876857+AST9QT@users.noreply.github.com>
2022-10-28 21:25:31 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,27 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    Y = receivedNumber / 5
+    X = receivedNumber % 5
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    led.plot(X, Y)
+})
+let Y = 0
+let X = 0
+radio.setGroup(5)
+X = 2
+Y = 2
+basic.showLeds(`
+    . . # . .
+    . . # . .
+    # # . # #
+    . . # . .
+    . . # . .
+    `)
 basic.forever(function () {
 	
 })
